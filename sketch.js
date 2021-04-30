100f043d3e9d92973845b3e7602a049cd33542d7
vaishali570 <76772213+vaishali570@users.noreply.github.com>
2021-04-30 07:35:25 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+const Engine=Matter.Engine;
+const World=Matter.World;
+const Bodies=Matter.Bodies;
+
+var snow;
+var boy;
+
+function preload(){
+ 
+}
+
+
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+  snow=new Snow(300,200);
+  boy=new Boy(50,200);
+
+}
+
+function draw() {
+  background(snow);  
+  
+  snow.addImage(snow);
+  boy.addImage(boy);
+  
+ for(vari=0;i<100;i++){
+   snow.display;
+ }
+ 
+ 
+ 
+  drawSprites();
+}
